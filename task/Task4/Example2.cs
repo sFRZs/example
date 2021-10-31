30c682b8a287a1df81d388ee92b6c57c4006eff9
imasltsev <92319013+imasltsev@users.noreply.github.com>
2021-10-31 22:20:42 +0300

    Update Example2.cs

--- task/Task4/Example2.cs
+++ task/Task4/Example2.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Task4._2
 {
@@ -7,14 +9,74 @@ namespace Task4._2
         static void Main(string[] args)
         {
 
+            //example with a queue
             int sum = 0;
+            int summa = 0;
+            //int summa1 = 0;
+            int[] arr = Enumerable.Range(1, 25).ToArray();
+            Console.WriteLine("Queue");
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+            int min1 = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] < min1)
+                {
+                    min1 = arr[i];
+
+                }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Element min " + min1);
+
+            int max1 = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] > max1)
+                {
+                    max1 = arr[i];
+
+                }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Element max " + max1);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (max1 > min1 && (i - 1) < max1)
+                {
+                    summa += arr[i];
+                }
+                if (max1 < min1 && (i - 1) < min1)
+                {
+                    summa += arr[i + 1];
+                }
+            }
+            int summa1 = (min1 + max1);
+            int summa2 = summa - summa1;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Sum all elements: " + summa);
+            Console.WriteLine("Sum max and min : " + summa1);
+            Console.WriteLine("Sum between the maximum and minimum: " + summa2);
+            Console.WriteLine("---------------------------------------------------------");
+            /*for (int i = Math.Min(min1, max1) ; i < Math.Max(min1, max1); i++)
+            {
+                summa1 += arr[i];
+            }
+            foreach (int i in arr)
+                Console.Write(i + " ");
+            Console.WriteLine("Сумма: " + summa1);
+            Console.WriteLine("----------------------------------------------------------");*/
+
+            //example with a random
+            Console.ForegroundColor = ConsoleColor.Gray;
             int[] ArrayA = new int[20];
             Random randA = new Random();
             Console.WriteLine("Random array A");
             for (int x = 0; x < ArrayA.Length; x++)
             {
                 ArrayA[x] = randA.Next(100);
-                Console.WriteLine("Element["+x+"]:"+ ArrayA[x]);
+                Console.WriteLine("Element[" + x + "]:" + ArrayA[x]);
             }
             int temp;
             for (int i = 0; i < ArrayA.Length - 1; i++)
@@ -32,16 +94,15 @@ namespace Task4._2
             Console.WriteLine("Output of a sorted array");
             for (int i = 0; i < ArrayA.Length; i++)
             {
-                Console.WriteLine("Element["+i+"]:"+ ArrayA[i]);
+                Console.WriteLine("Element[" + i + "]:" + ArrayA[i]);
             }
 
             int min = ArrayA[0];
-            
+
             for (int i = 0; i < ArrayA.Length; i++)
                 if (ArrayA[i] < min)
                 {
                     min = ArrayA[i];
-                  
                 }
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Green;
@@ -49,10 +110,10 @@ namespace Task4._2
 
             int max = ArrayA[0];
             for (int i = 0; i < ArrayA.Length; i++)
-                if (ArrayA[i]>  max)
+                if (ArrayA[i] > max)
                 {
                     max = ArrayA[i];
-                    
+
                 }
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Red;
@@ -64,7 +125,7 @@ namespace Task4._2
                 }
           foreach (int i in ArrayA)
                   Console.Write(i + " ");*/
-            
+
             for (int i = 0; i < ArrayA.Length; i++)
             {
                 if (max > min && (i - 1) < max)
