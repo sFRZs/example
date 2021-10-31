0306840c987edfcdc6717a0f8e1d14e66d474a25
imasltsev <92319013+imasltsev@users.noreply.github.com>
2021-10-31 17:22:46 +0300

    Add files via upload

---
+++ task/Task4/Example2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Task4._2
+{
+    class Example2
+    {
+        static void Main(string[] args)
+        {
+
+            int sum = 0;
+            int[] ArrayA = new int[20];
+            Random randA = new Random();
+            Console.WriteLine("Random array A");
+            for (int x = 0; x < ArrayA.Length; x++)
+            {
+                ArrayA[x] = randA.Next(100);
+                Console.WriteLine("Element["+x+"]:"+ ArrayA[x]);
+            }
+            int temp;
+            for (int i = 0; i < ArrayA.Length - 1; i++)
+            {
+                for (int j = i + 1; j < ArrayA.Length; j++)
+                {
+                    if (ArrayA[i] > ArrayA[j])
+                    {
+                        temp = ArrayA[i];
+                        ArrayA[i] = ArrayA[j];
+                        ArrayA[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Output of a sorted array");
+            for (int i = 0; i < ArrayA.Length; i++)
+            {
+                Console.WriteLine("Element["+i+"]:"+ ArrayA[i]);
+            }
+
+            int min = ArrayA[0];
+            
+            for (int i = 0; i < ArrayA.Length; i++)
+                if (ArrayA[i] < min)
+                {
+                    min = ArrayA[i];
+                  
+                }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Element min " + min);
+
+            int max = ArrayA[0];
+            for (int i = 0; i < ArrayA.Length; i++)
+                if (ArrayA[i]>  max)
+                {
+                    max = ArrayA[i];
+                    
+                }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Element max " + max);
+            /*   for (int i = Math.Min(max, min)+1 ; i < Math.Max(max, min); i++)
+                {
+                    Console.WriteLine(i);
+                    sum +=ArrayA[i]; 
+                }
+          foreach (int i in ArrayA)
+                  Console.Write(i + " ");*/
+            
+            for (int i = 0; i < ArrayA.Length; i++)
+            {
+                if (max > min && (i - 1) < max)
+                {
+                    sum += ArrayA[i];
+                }
+                if (max < min && (i - 1) < min)
+                {
+                    sum += ArrayA[i + 1];
+                }
+            }
+            int sum1 = (min + max);
+            int sum2 = sum - sum1;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Sum all elements: " + sum);
+            Console.WriteLine("Sum max and min : " + sum1);
+            Console.WriteLine("Sum between the maximum and minimum: " + sum2);
+        }
+    }
+}
---
+++ task/Task4/Exmaple1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace Task4._1
+{
+    class Exmaple1
+    {
+        static int[] ConcatArrays(int[] ArrayA, int[] ArrayB)
+        {
+            int[] ArrayC = new int[ArrayA.Length + ArrayB.Length];////the length of the resulting array is equal to the sum of the lengths of the original arrays
+            for (int i = 0; i < ArrayA.Length; i++)//transferring numbers from array A to mass C
+
+                ArrayC[i] = ArrayA[i];
+
+            for (int i = ArrayA.Length; i < ArrayA.Length + ArrayB.Length; i++)//we transfer numbers from array B to mass C, starting from index ArrayA.Length
+
+                ArrayC[i] = ArrayB[i - ArrayA.Length];
+            return ArrayC;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] ArrayA = new int[5];
+            Random randA = new Random();
+            Console.WriteLine("Random array A");
+            for (int x = 0; x < ArrayA.Length; x++)
+            {
+                ArrayA[x] = randA.Next(20);
+                Console.WriteLine(ArrayA[x]);
+            }
+            int[] ArrayB = new int[5];
+            Random randB = new Random();
+            Console.WriteLine("Random array B");
+            for (int y = 0; y < ArrayA.Length; y++)
+            {
+                ArrayB[y] = randB.Next(20);
+                Console.WriteLine(ArrayB[y]);
+            }
+            int[] ArrayC = ConcatArrays(ArrayA, ArrayB);
+
+            int temp;// sort in ascending order ArrayC
+            for (int i=0; i < ArrayC.Length - 1; i++)
+            {
+                for (int j = i + 1; j < ArrayC.Length; j++)
+                {
+                    if (ArrayC[i] > ArrayC[j])
+                    {
+                        temp = ArrayC[i];
+                        ArrayC[i] = ArrayC[j];
+                        ArrayC[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("array A to join with array B ");
+            foreach (int i in ArrayC)
+                Console.WriteLine($"{i}");
+        }
+    }
+}
